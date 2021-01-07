c54396d37fed6e33a2114146f781cacc07a3f942
Gabriel Da Silva Souza <gabriel.ssouza@totvs.com.br>
2021-01-07 13:51:14 -0300

    Initial exercises

---
+++ EstruturaSequencial/C/Exe2/main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    char Numero[20];
+
+    printf("Digite um numero: ");
+    scanf("%s",Numero);
+    printf("O numero informado foi: %s \n",Numero);
+
+    return 0;
+}
---
+++ EstruturaSequencial/C/Exe3/main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    int Numero1,Numero2,Resultado;
+
+    printf("Digite um numero: ");
+    scanf("%i",&Numero1);
+    printf("Digite um numero: ");
+    scanf("%i",&Numero2);
+
+    Resultado = Numero1 + Numero2;
+
+    printf("A Soma dos numeros é: %i \n", Resultado);
+
+    return 0;
+}
